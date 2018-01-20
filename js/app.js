7a8faed2e80c6ebf898cb64bd248e7a6de71337a
Colin <colinmaher123@gmail.com>
2018-01-20 04:11:23 -0800

    added dotenv package to hide private keys and updated package.json and .gitignore

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
+
+//sets environment variables in .env file
+require('dotenv').config();
+
 const express = require('express');
-const OT = require('opentok');
+const OpenTok = require('opentok');
+const path = require('path');
 const app = express();
 const port = 8080;
 
-app.listen(port, () => console.log('OnlineOfficeHours listening on ' + port));
-
-const apiKey = '46042852';
-const apiSecret = '94fd8608d10645d74c3c76263c8a93e0d31ea885';
-//let token = 'T1==cGFydG5lcl9pZD00NTgyODA2MiZzaWc9ZThiNzcyZTZjY2RiZDI1MTYwNDMyNzEzYmFkOTE1ZDMyZDIwYTFlZjpzZXNzaW9uX2lkPTFfTVg0ME5UZ3lPREEyTW41LU1UVXhOalF6T0RRNE1URTFNSDQzVDBNeU1FNTVaVmxxZVVFeWNtbEZaMFZGVjBSRWRYZC1VSDQmY3JlYXRlX3RpbWU9MTUxNjQzODQ4MyZub25jZT0wLjk4NzgzOTE0OTIyMDY2NDQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUxNjUyNDg4Mw==';
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
 const opentok = new OpenTok(apiKey, apiSecret);
 
-let session = opentok.createSession(function (err, session) {
-    if (err) return console.log(err);
-});
+app.listen(port, () => console.log('OnlineOfficeHours listening on ' + port));
 
-app.get('/', (req, res) => res.sendFile('./index.html'));
+
+
+// let session = opentok.createSession(function (err, session) {
+//     if (err) return console.log(err);
+// });
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
