7254ca70547bf736d0e724ce140309603ebc180a
dunixify <vmelkote@ucsc.edu>
2018-01-20 09:17:01 -0800

    commented out dotenv and opentok as the server won't run without feeding in the key and secret and the dotenv is not functional

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
+//import { request } from 'http';
 
 //sets environment variables in .env file
-require('dotenv').config();
+//require('dotenv').config();
 
 const express = require('express');
 const OpenTok = require('opentok');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
 const apiKey = process.env.API_KEY;
 const apiSecret = process.env.API_SECRET;
-const opentok = new OpenTok(apiKey, apiSecret);
-
-app.listen(port, () => console.log('OnlineOfficeHours listening on ' + port));
-
-
+//const opentok = new OpenTok(apiKey, apiSecret);
 
 // let session = opentok.createSession(function (err, session) {
 //     if (err) return console.log(err);
 // });
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../html/index.html')));
+
+app.listen(port, () => console.log('OfficeHoursOnline listening on ' + port));
